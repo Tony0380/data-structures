fab9b0da6fa8a7654a25667e38e1b93f8428001f
Antonio Colamartino <colamar95@outlook.it>
2024-07-29 11:34:15 +0200

    Update Pila

---
+++ data_structures/Pila/Stack.h
@@ -0,0 +1,22 @@
+//
+// Created by Colam on 29/07/2024.
+//
+
+#ifndef DATA_STRUCTURES_STACK_H
+#define DATA_STRUCTURES_STACK_H
+
+#include <iostream>
+
+template <typename T,typename P>
+class Stack {
+public:
+    typedef T tipo_elem;
+    typedef P posizione;
+
+    virtual void creapila() = 0;
+    virtual bool pilavuota() const = 0;
+    virtual tipo_elem leggipila() const = 0;
+    virtual void fuoripila() = 0;
+    virtual void inpila(const tipo_elem&) = 0;
+};
+#endif //DATA_STRUCTURES_STACK_H
