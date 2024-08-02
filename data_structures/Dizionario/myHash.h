9cde1093463d871dc982d8614e6c357b6906ba37
Antonio Colamartino <colamar95@outlook.it>
2024-08-02 16:02:36 +0200

    Update

---
+++ data_structures/Dizionario/myHash.h
@@ -0,0 +1,35 @@
+//
+// Created by Colam on 02/08/2024.
+//
+
+#ifndef DATA_STRUCTURES_MYHASH_H
+#define DATA_STRUCTURES_MYHASH_H
+
+#include <string>
+#include <stdexcept>
+#include <ostream>
+#include <string>
+
+using std::string;
+
+template<typename T>
+class myHash {
+public:
+    size_t operator() (const T Key) {
+        return size_t (Key);
+    }
+};
+
+//polimorfismo parametrico ad hoc per il tipo stringa
+template<>
+class myHash<string> {
+    size_t operator() (const string &Key) {
+        unsigned long hash_value = 0;
+        int length = (int) Key.length ();
+        for (int i = 0; i < length; i++)
+            hash_value = 5 * hash_value + Key.at (i);
+        return size_t (hash_value);
+    }
+};
+
+#endif //DATA_STRUCTURES_MYHASH_H
