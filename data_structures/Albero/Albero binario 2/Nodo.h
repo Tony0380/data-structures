96fa10a365cc600dfd1b250c3ca9649b9bd69df9
Antonio Colamartino <colamar95@outlook.it>
2024-08-03 18:41:20 +0200

    Update

--- data_structures/Albero/Albero binario 2/Nodo.h
+++ data_structures/Albero/Albero binario 2/Nodo.h
@@ -6,59 +6,65 @@
 #define DATA_STRUCTURES_NODO_H
 
 
-template <typename T>
+template<typename T>
 class Nodo {
 public:
     typedef T tipo_elem;
 
-    Nodo();
+    Nodo ();
 
-    Nodo* getSinistro() const;
-    Nodo* getDestro() const;
-    void setSinistro(Nodo*);
-    void setDestro(Nodo*);
-    tipo_elem getElem() const;
-    void setElem(const tipo_elem &);
+    Nodo *getSinistro () const;
+
+    Nodo *getDestro () const;
+
+    void setSinistro (Nodo *);
+
+    void setDestro (Nodo *);
+
+    tipo_elem getElem () const;
+
+    void setElem (const tipo_elem &);
 
 private:
-    Nodo* figlioSinistro;
-    Nodo* figlioDestro;
+    Nodo *figlioSinistro;
+    Nodo *figlioDestro;
     tipo_elem Elemento;
 };
 
 template<typename T>
-void Nodo<T>::setElem(const tipo_elem &newElem) {
+void Nodo<T>::setElem (const tipo_elem &newElem) {
     Elemento = newElem;
 }
 
 template<typename T>
-void Nodo<T>::setDestro(Nodo *newDestro) {
+void Nodo<T>::setDestro (Nodo *newDestro) {
     figlioDestro = newDestro;
 }
 
 template<typename T>
-void Nodo<T>::setSinistro(Nodo *newSinistro) {
+void Nodo<T>::setSinistro (Nodo *newSinistro) {
     figlioSinistro = newSinistro;
 }
 
 template<typename T>
-typename Nodo<T>::tipo_elem Nodo<T>::getElem() const {
+typename Nodo<T>::tipo_elem Nodo<T>::getElem () const {
     return Elemento;
 }
 
 template<typename T>
-Nodo<T> *Nodo<T>::getDestro() const {
+Nodo<T> *Nodo<T>::getDestro () const {
     return figlioDestro;
 }
 
 template<typename T>
-Nodo<T> *Nodo<T>::getSinistro() const {
+Nodo<T> *Nodo<T>::getSinistro () const {
     return figlioSinistro;
 }
 
 template<typename T>
-Nodo<T>::Nodo() {
+Nodo<T>::Nodo () {
     figlioDestro = nullptr;
     figlioSinistro = nullptr;
 }
+
 #endif //DATA_STRUCTURES_NODO_H
--- data_structures/Albero/Albero binario 2/bintree.h
+++ data_structures/Albero/Albero binario 2/bintree.h
@@ -9,27 +9,41 @@
 #include "Nodo.h"
 
 
-template <class T, class N>
+template<class T, class N>
 class bintree {
 public:
     typedef T tipo_elem;
     typedef N nodo;
 
-    virtual void creabinalbero() = 0;
-    virtual bool binalberovuoto() const = 0;
-    virtual nodo binradice() const = 0;
-    virtual nodo binpadre() const = 0;
-    virtual nodo figliosinistro(nodo) const = 0;
-    virtual nodo figliodestro(nodo) const = 0;
-    virtual bool sinistrovuoto(nodo) const = 0;
-    virtual bool destrovuoto(nodo) const = 0;
-    virtual bintree costrbinalbero(bintree) const = 0;
-    virtual void cancsottobinalbero(nodo) = 0;
-    virtual tipo_elem legginodo(nodo) const = 0;
-    virtual void scrivinodo(tipo_elem, nodo) = 0;
-    virtual void insbinradice(nodo) = 0;
-    virtual void insfigliosinistro(nodo) = 0;
-    virtual void insfigliodestro(nodo) = 0;
+    virtual void creabinalbero () = 0;
+
+    virtual bool binalberovuoto () const = 0;
+
+    virtual nodo binradice () const = 0;
+
+    virtual nodo binpadre () const = 0;
+
+    virtual nodo figliosinistro (nodo) const = 0;
+
+    virtual nodo figliodestro (nodo) const = 0;
+
+    virtual bool sinistrovuoto (nodo) const = 0;
+
+    virtual bool destrovuoto (nodo) const = 0;
+
+    virtual bintree costrbinalbero (bintree) const = 0;
+
+    virtual void cancsottobinalbero (nodo) = 0;
+
+    virtual tipo_elem legginodo (nodo) const = 0;
+
+    virtual void scrivinodo (tipo_elem, nodo) = 0;
+
+    virtual void insbinradice (nodo) = 0;
+
+    virtual void insfigliosinistro (nodo) = 0;
+
+    virtual void insfigliodestro (nodo) = 0;
 
 };
 
