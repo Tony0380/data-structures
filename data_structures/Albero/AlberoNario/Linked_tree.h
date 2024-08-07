8667fdfece50c5f7dc2808a194687adc6af06321
Antonio Colamartino <colamar95@outlook.it>
2024-08-07 12:33:54 +0200

    Update

--- data_structures/Albero/AlberoNario/Linked_tree.h
+++ data_structures/Albero/AlberoNario/Linked_tree.h
@@ -7,4 +7,40 @@
 
 #include "tree.h"
 
+template<class T>
+class Linked_tree : public tree<T, Nodo<T> *> {
+public:
+    typedef typename bintree<T, Nodo<T> *>::tipo_elem tipo_elem;
+    typedef typename bintree<T, Nodo<T> *>::nodo nodo;
+
+    //costruttore vuoto
+    Linked_tree();
+
+    //costruttore di copia
+    Linked_tree(const Linked_tree &);
+
+    //distruttore
+    ~Linked_tree();
+
+    //ridefinizione dei metodi virtuali puri della classe tree
+
+    void creaalbero();
+
+    bool alberovuoto() const;
+
+    void insradice(nodo);
+
+    nodo radice() const;
+
+    nodo padre(nodo) const;
+
+    bool foglia(nodo) const;
+
+    nodo primofiglio(nodo) const;
+
+    bool ultimofratello(nodo) const;
+
+    nodo succfratello(nodo) const;
+};
+
 #endif //DATA_STRUCTURES_LINKED_TREE_H
