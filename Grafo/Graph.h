b44dc276938f97df0a64b60839912768dfd6cdb7
Antonio Colamartino <32678450+Tony0380@users.noreply.github.com>
2024-10-23 17:07:58 +0200

    Update Grafo

--- Grafo/Graph.h
+++ Grafo/Graph.h
@@ -5,4 +5,18 @@
 #ifndef DATA_STRUCTURES_GRAPH_H
 #define DATA_STRUCTURES_GRAPH_H
 
+#include <iostream>
+#include "Nodo.h"
+
+template <typename T>
+class Graph {
+public:
+    typedef T tipoelem;
+
+    virtual void creagrafo();
+    virtual bool vuoto();
+    virtual void insnodo(nodo);
+
+};
+
 #endif //DATA_STRUCTURES_GRAPH_H
---
+++ Grafo/Nodo.h
@@ -0,0 +1,21 @@
+//
+// Created by tony on 23/10/24.
+//
+
+#ifndef DATA_STRUCTURES_NODO_H
+#define DATA_STRUCTURES_NODO_H
+
+#include <iostream>
+
+template<typename T>
+class Nodo {
+public:
+    typedef T tipoelem;
+
+    void scrivinodo(tipoelem);
+    tipoelem legginodo();
+    void
+};
+
+
+#endif //DATA_STRUCTURES_NODO_H
